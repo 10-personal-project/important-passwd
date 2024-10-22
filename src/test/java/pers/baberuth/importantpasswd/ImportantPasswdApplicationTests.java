b18283f9f46233d1eea41cde3aa8630914146131
Rayonlar <taycancille@outlook.com>
2024-10-22 21:46:18 +0800

    feat: 初始化

---
+++ src/test/java/pers/baberuth/importantpasswd/ImportantPasswdApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.baberuth.importantpasswd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportantPasswdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
